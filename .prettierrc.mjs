b3957d54ce9fb6ea5202d9f2a7e98da894a21c38
Slawek Kolodziej <hfrntt@gmail.com>
2025-03-19 14:39:49 +0100

    refactor: update prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  offsetTernaryExpressions: true
+};
